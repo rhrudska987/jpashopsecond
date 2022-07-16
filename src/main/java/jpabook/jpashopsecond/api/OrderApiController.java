3f5b4708b4e1d19aba3e631a96ec0d5a621174d4
KyungNam Ko <rhrudska987@naver.com>
2022-07-16 18:16:58 +0900

    2022.07.16 Study 실전! 스프링 부트와 JPA 활용2 - API 개발과 성능 최적화 API 개발 고급 - 컬렉션 조회 최적화  * 1. 엔티티를 조회해서 그대로 반환: V1  * 2. 엔티티 조회 후 DTO 변환: V2  * 3. 페치 조인으로 쿼리 수 최적화: V3  * 4. 컬렌션 페이징과 한계 돌파: V3.1  * -컬렉션은 페치 조인시 페이징 불가능  * -ToOne 관계는 페치 조인으로 쿼리 수 최적화  * -컬렉션은 페치 조인 대신에 지연 로딩을 유지하고, hibernate.default_batch_fetch_size, @BatchSize로 최적화  * DTO 직접 조회  * 5. JPA에서 DTO를 직접 조회: V4  * 6. 컬렉션 조회 최적화 - 일대다 관계인 컬렉션은 IN 절을 활용해서 메모리에 미리 조회해서 최적화: V5  * 7. 플랫 데이터 최적화 - JOIN 결과를 그대로 조회 후 애플리케이션에서 원하는 모양으로 직접 변환: V6  *  * 권장 순서  * 엔티티 조회 방식으로 우선 접근  * 1. 페치조인으로 쿼리 수를 최적화  * 2. 컬렉션 최적화  *  1) 페이징 필요 hibernate.default_batch_fetch_size, @BatchSize로 최적화  *  2) 페이징 필요X -> 페치 조인 사용  * !!엔티티 조회 방식으로 해결이 안되면 DTO 조회 방식 사용
     *  OSIV의 경우 고객 서비스의 실시간 API는 OSIV를 끄고, ADMIN 처럼 커넥션을 많이 사용하지 않는 곳에서는 OSIV를 켠다.

--- src/main/java/jpabook/jpashopsecond/api/OrderApiController.java
+++ src/main/java/jpabook/jpashopsecond/api/OrderApiController.java
@@ -6,22 +6,47 @@ import jpabook.jpashopsecond.domain.OrderItem;
 import jpabook.jpashopsecond.domain.OrderStatus;
 import jpabook.jpashopsecond.repository.OrderRepository;
 import jpabook.jpashopsecond.repository.OrderSearch;
-import lombok.Data;
+import jpabook.jpashopsecond.repository.order.query.OrderFlatDto;
+import jpabook.jpashopsecond.repository.order.query.OrderQueryDto;
+import jpabook.jpashopsecond.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+/**
+ * 1. 엔티티를 조회해서 그대로 반환: V1
+ * 2. 엔티티 조회 후 DTO 변환: V2
+ * 3. 페치 조인으로 쿼리 수 최적화: V3
+ * 4. 컬렌션 페이징과 한계 돌파: V3.1
+ * -컬렉션은 페치 조인시 페이징 불가능
+ * -ToOne 관계는 페치 조인으로 쿼리 수 최적화
+ * -컬렉션은 페치 조인 대신에 지연 로딩을 유지하고, hibernate.default_batch_fetch_size, @BatchSize로 최적화
+ * DTO 직접 조회
+ * 5. JPA에서 DTO를 직접 조회: V4
+ * 6. 컬렉션 조회 최적화 - 일대다 관계인 컬렉션은 IN 절을 활용해서 메모리에 미리 조회해서 최적화: V5
+ * 7. 플랫 데이터 최적화 - JOIN 결과를 그대로 조회 후 애플리케이션에서 원하는 모양으로 직접 변환: V6
+ *
+ * 권장 순서
+ * 엔티티 조회 방식으로 우선 접근
+ * 1. 페치조인으로 쿼리 수를 최적화
+ * 2. 컬렉션 최적화
+ *  1) 페이징 필요 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+ *  2) 페이징 필요X -> 페치 조인 사용
+ * !!엔티티 조회 방식으로 해결이 안되면 DTO 조회 방식 사용
+ */
 
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -49,6 +74,30 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
         return result;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+
+        List<OrderDto> result = orders.stream().map(o -> new OrderDto(o)).collect(Collectors.toList());
+        return result;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderFlatDto> ordersV6(){
+        return orderQueryRepository.findAllByDto_flat();
     }
 
     @Getter
--- src/main/java/jpabook/jpashopsecond/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashopsecond/repository/OrderRepository.java
@@ -104,6 +104,14 @@ public class OrderRepository {
                         " join fetch o.delivery d", Order.class).getResultList();
     }
 
+    public List<Order> findAllWithMemberDelivery(int offset, int limit){
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset).setMaxResults(limit).getResultList();
+    }
+
     public List<Order> findAllWithItem() { //단점은 페이징이 안됨
         return em.createQuery(
                 "select distinct o from Order o" +
